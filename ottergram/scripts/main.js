5a23e4e97837ef1694f18c771049371df8aa68e3
Thomas Ngo <tngo0508@gmail.com>
2018-02-24 04:08:01 -0800

    Finished clicking < and > cycle left and right through thumbnails, detail images, and titles

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,6 +1,7 @@
 var DETAIL_IMAGE_SELECTOR = "[data-image-role=target]";
 var DETAIL_TITLE_SELECTOR = "[data-image-role=title]";
 var THUMBNAIL_LINK_SELECTOR = "[data-image-role=trigger]";
+var current = 0;
 
 function setDetails(imageUrl, titleText) {
   "use strict";
@@ -43,7 +44,25 @@ function getThumbnailsArray() {
 function initializeEvents() {
   "use strict";
   var thumbnails = getThumbnailsArray();
-  thumbnails.forEach(addThumbClickHandler);
+  thumbnails.forEach(function(thumb, index) {
+    thumb.addEventListener("click", function(event) {
+      event.preventDefault();
+      if (index == 5) {
+        if (current <= 0) {
+          current = 5;
+        }
+        thumb = thumbnails[(current - 1) % 5];
+        current--;
+      } else if (index == 6) {
+        if (current >= 4) {
+          current = -1;
+        }
+        thumb = thumbnails[(current + 1) % 5];
+        current++;
+      }
+      setDetailsFromThumb(thumb);
+    });
+  });
 }
 
 initializeEvents();
