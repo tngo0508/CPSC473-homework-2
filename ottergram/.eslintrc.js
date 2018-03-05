fe8a196a26d3cf4daaeef68f4af4f0f7166e0a2f
Thomas Ngo <tngo0508@gmail.com>
2018-03-04 23:21:27 -0800

    fix console.log() error

--- ottergram/.eslintrc.js
+++ ottergram/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "rules": {
+        "no-console": 0,
         "indent": [
             "error",
             2
@@ -22,4 +23,3 @@ module.exports = {
         ]
     }
 };
-
